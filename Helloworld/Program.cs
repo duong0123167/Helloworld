b3f8aa13b1bfce0f782478f08edb26a03d78c756
duongnguyenhong2004 <duongnguyenhong2004@gmail.com>
2025-01-23 13:51:31 +0700

    luu du lieu 23/1/2015 -them console.writeline

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -11,6 +11,7 @@ namespace Helloworld
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
+            Console.WriteLine("hello again");
         }
     }
 }
